515bfd5b51058f6c03cb011f30cd320b2a8effb3
kj <kristoferjoseph@gmail.com>
2020-09-21 12:41:06 -0700

    Updates to sandbox 2.0. Adds support for AWS HTTP APIs

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -18,7 +18,11 @@ let handler = server.createHandler()
 
 exports.handler = function(event, context, callback) {
   let body = arc.http.helpers.bodyParser(event)
+  // Support for AWS HTTP API syntax
+  event.httpMethod = event.httpMethod
+    ? event.httpMethod
+    : event.requestContext.http.method
   // Body is now parsed, re-encode to JSON for Apollo
-  event.body = JSON.stringify(body)
+  event.body = body
   handler(event, context, callback)
 }
