9f18299de9b9c8d0e59dadcf7de1e26a34fb7dc9
kj <kristoferjoseph@gmail.com>
2020-11-06 10:40:47 -0800

    Updates sandbox

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -18,11 +18,12 @@ let handler = server.createHandler()
 
 exports.handler = function(event, context, callback) {
   let body = arc.http.helpers.bodyParser(event)
+  console.log(body)
   // Support for AWS HTTP API syntax
   event.httpMethod = event.httpMethod
     ? event.httpMethod
     : event.requestContext.http.method
   // Body is now parsed, re-encode to JSON for Apollo
-  event.body = body
+  event.body = JSON.stringify(body)
   handler(event, context, callback)
 }
